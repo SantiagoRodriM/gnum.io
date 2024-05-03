59244944f7bef664e74c9e9a2780f27e571baadb
SantiagoRodriM <145405929+SantiagoRodriM@users.noreply.github.com>
2024-05-03 11:02:06 -0500

    Delete js directory

--- js/credenciales.js
+++
@@ -1,4 +0,0 @@
-// TODO(developer): Set to client ID and API key from the Developer Console
-const CLIENT_ID = '786547057809-n8ci9vtak1qiog0mgkjjk4cqvl44l5sc.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyCq0Vm4T1OWNsdatm9X9CSVoaK0Nj7XGgw';
-const SPREADSHEET = "1KFaFd9sutHdGzWw8nkje3FmK4V9OXa06Z4rgsEz2vRw";
